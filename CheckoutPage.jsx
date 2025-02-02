494cf61f04b3c0623d1501787b7ed0c0302f9c97
Wala2324 <walajuma01@gmail.com>
2025-02-02 20:44:25 +0100

    Add files via upload

--- CheckoutPage.jsx
+++ CheckoutPage.jsx
@@ -1,6 +1,12 @@
-const CheckoutPage =() => {
+import ShoppingCart from "../components/ShppingCart"
+
+const CheckoutPage = () => {
     return(
-        <div>CheckoutPage</div>
+        <div className="mt-10">
+          <div className="bg-white rounded-lg">
+          <ShoppingCart isCheckoutPage />
+          </div>
+        </div>
     )
 }
 export default CheckoutPage
